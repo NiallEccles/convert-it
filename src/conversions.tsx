f7770593aa0e5a9a2b984ae9996e159fa25dd161
Dev <n.eccles14@gmail.com>
2023-04-20 23:05:25 +0100

    Cleanup conversions file.

--- src/conversions.tsx
+++ src/conversions.tsx
@@ -1,12 +1,10 @@
 export interface Conversion {
   name: string;
-  //   conversions: Map<string, Unit>;
   units: Unit[];
 }
 
 export interface Unit {
   symbol: string;
-  //   conversions: Map<string, Function>;
   icon: string;
   name: string;
   to: Formula[];
@@ -16,27 +14,6 @@ export interface Formula {
   name: string;
   formula: Function;
 }
-
-export const celsius = {
-  symbol: "°C",
-  conversions: new Map([
-    ["Fahrenheit", (celsius: number) => (celsius * 9) / 5 + 32],
-    ["Kelvin", (celsius: number) => celsius + 273.15],
-  ]),
-};
-
-export const fahrenheit = {
-  symbol: "°F",
-  conversions: new Map([
-    ["Celsius", (fahrenheit: number) => ((fahrenheit - 32) * 5) / 9],
-    ["Kelvin", (fahrenheit: number) => ((fahrenheit - 32) * 5) / 9 + 273.15],
-  ]),
-};
-
-// export const temperature: Map<string, Unit> = new Map([
-//   ["Celsius", celsius],
-//   ["Fahrenheit", fahrenheit],
-// ]);
 
 export const temperature: Unit[] = [
   {
